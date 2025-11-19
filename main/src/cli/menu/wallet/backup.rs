d800e242a5629ead133e6dc472a9bd2b23801608
volodymyrd <volodymyrdotsenko@gmail.com>
2025-11-19 10:59:33 +0100

    use password input fow sensitive data.

--- main/src/cli/menu/wallet/backup.rs
+++ main/src/cli/menu/wallet/backup.rs
@@ -13,9 +13,9 @@ impl BackupWallet {
         term: &Term,
         wallet: &EncryptedWallet,
     ) -> Result<(), anyhow::Error> {
-        let master_password: String = dialoguer::Input::with_theme(theme)
+        let master_password: String = dialoguer::Password::with_theme(theme)
             .with_prompt("Enter a master password or 'q' to quit")
-            .interact_text_on(term)?;
+            .interact_on(term)?;
         if master_password != "q" {
             let mnemonic = ConsoleService::new()
                 .backup_wallet(wallet, master_password)
--- main/src/cli/menu/wallet/open_wallet.rs
+++ main/src/cli/menu/wallet/open_wallet.rs
@@ -16,9 +16,9 @@ impl OpenWallet {
             .with_prompt("Enter a wallet name or 'q' to quit")
             .interact_text_on(term)?;
         if name != "q" {
-            let master_password: String = dialoguer::Input::with_theme(theme)
+            let master_password: String = dialoguer::Password::with_theme(theme)
                 .with_prompt("Enter a master password or 'q' to quit")
-                .interact_text_on(term)?;
+                .interact_on(term)?;
             if master_password != "q" {
                 return match ConsoleService::new()
                     .open_wallet(name.as_str(), master_password)
